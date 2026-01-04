e7c877b74986c8e1d5dab36d2c1fc99fba647341
Jonathan Melly <jonathan.melly@gmail.com>
2026-01-04 07:32:32 +0100

    feat(gulp): use specific compatible node version without switching system

--- gulp.bat
+++ gulp.bat
@@ -1,1 +1,10 @@
-node_modules\.bin\gulp %*
+@echo off
+REM Use custom node as old project...
+set "NODE_VERSION=20.6.0"
+set "NVM_SYMLINK=%NVM_HOME%\v%NODE_VERSION%"
+
+REM Temporarily add the specific Node version to PATH
+set "PATH=%NVM_SYMLINK%;%PATH%"
+
+REM Call gulp.cmd which will use the node from PATH
+call node_modules\.bin\gulp.cmd %*
